6db24ed97010616b199b50f7382a02ee8fb8c1f3
Richard <liuyngchng@hotmail.com>
2020-03-04 20:20:42 +0800

    add timer

--- test/timer.c
+++ test/timer.c
@@ -6,9 +6,9 @@ int handle_count=0;
 void set_time(void)
 {
    struct itimerval itv;
-   itv.it_interval.tv_sec=10;	//自动装载，之后每10秒响应一次
-   itv.it_interval.tv_usec=0;
-   itv.it_value.tv_sec=5;       //第一次定时的时间
+   itv.it_interval.tv_sec=0;	        //单位为秒,     自动装载，之后每10秒响应一次
+   itv.it_interval.tv_usec=100*1000;    //单位为微秒.
+   itv.it_value.tv_sec=1;               //第一次定时的时间
    itv.it_value.tv_usec=0;
    setitimer(ITIMER_REAL,&itv,NULL);
 }
@@ -16,7 +16,8 @@ void set_time(void)
 void alarm_handle(int sig)
 {
    handle_count++;
-   printf("have handle count is %d\n",handle_count);
+   time_t t = time(NULL);
+   printf("%ld,have handle count is %d\n",t,handle_count);
 }
  
 int main(void)
@@ -25,12 +26,12 @@ int main(void)
    	signal(SIGALRM,alarm_handle);
    	set_time();
    	char *buf;
-    //gets(buf);
-    while(1)
-	{
-   		getitimer(ITIMER_REAL,&itv);
-		printf("pass second is %d\n",(int)itv.it_value.tv_sec);
-   		sleep(1);
-   	}	
+    gets(buf);
+    //while(1)
+	//{
+   	//	getitimer(ITIMER_REAL,&itv);
+	//	printf("pass second is %d\n",(int)itv.it_value.tv_sec);
+   	//	sleep(1);
+   	//}	
    	return 0;
 }
