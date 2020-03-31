2c784ddac62081dd317b962737b9bf5181153cd2
richard <liuyngchng@hotmail.com>
2020-03-31 15:45:04 +0800

    debug

--- udt/udt_server.cc
+++ udt/udt_server.cc
@@ -10,9 +10,12 @@
 #define _BUF_SIZE_ 8096
 using namespace std;
 
+void config_socket_opt(UDTSOCKET sockfd);
+
 int main()
 {
     UDTSOCKET sockfd = UDT::socket(AF_INET, SOCK_STREAM, 0);
+    config_socket_opt(sockfd);
     sockaddr_in my_addr;
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(_PORT);
@@ -58,4 +61,26 @@ int main()
     }
     UDT::close(sockfd);
     return 0;
+}
+
+void config_socket_opt(UDTSOCKET fd)
+{
+    int block[5];
+    int size = sizeof(int);
+    UDT::getsockopt(fd, 0, UDT_SNDBUF, &block[0], &size);
+    UDT::getsockopt(fd, 0, UDT_RCVBUF, &block[1], &size);
+    UDT::getsockopt(fd, 0, UDT_FC, &block[2], &size);
+    UDT::getsockopt(fd, 0, UDT_MSS, &block[3], &size);
+    UDT::getsockopt(fd, 0, UDT_SNDTIMEO, &block[4], &size);
+    cout << "socket option" << endl;
+    cout << "UDT_SNDBUF = " << block[0] << endl << "UDT_RCVBUF = " << block[1] << endl;
+    cout << "UDT_FC = " << block[2] << endl << "UDT_MSS = "<< block[3] << endl;
+    cout << "UDT_SNDTIMEO = " << block[4] << endl;
+    cout << "start config socket option." << endl;
+    int bdp = 100000;   //1000Mbps*1ms=1000Mb*10^-3=1Mb=10^6b=0.1^10^6B=100000B
+    int a = 25000;
+   // UDT::setsockopt(fd, 0, UDT_FC, &a, sizeof(int));
+    UDT::getsockopt(fd, 0, UDT_FC, &block[2], &size);
+    cout << "UDT_FC = "<< block[2] << endl;
+    return; 
 }
